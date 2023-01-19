98a3f10b055fcf3af1c19093ac9b409d6fa9a253
Lurius-tharn <sajous.davenese@gmail.com>
2023-01-19 01:22:05 +0100

    initialisation du backend

---
+++ src/main/java/fr/repairit/repairitbackend/RepairItBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.repairit.repairitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairItBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/repairit/repairitbackend/RepairItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.repairit.repairitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
