f963ba092fe424ad8c46cc1816b4171909a29d7a
Davenese <sajous.davenese@gmail.com>
2023-01-25 16:30:06 +0100

    suppression liquidbase

--- src/main/java/fr/repairitbackend/RepairItBackendApplication.java
+++ src/main/java/fr/repairitbackend/RepairItBackendApplication.java
@@ -2,8 +2,10 @@ package fr.repairitbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RepairItBackendApplication {
 
     public static void main(String[] args) {
